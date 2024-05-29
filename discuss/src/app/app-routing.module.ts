bbd7a030641ac5f59624d32982de1fb4d06526c2
Daniel <dannyj172@gmail.com>
2024-05-29 20:59:44 +0300

    auth, post and create pages styled

--- discuss/src/app/app-routing.module.ts
+++ discuss/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { PostComponent } from './components/pages/post/post.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { RegisterComponent } from './components/pages/register/register.component';
+import { CreatePostComponent } from './components/pages/create-post/create-post.component';
+import { TopicComponent } from './components/pages/topic/topic.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'post', component: PostComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'create-post', component: CreatePostComponent },
+  { path: 'topic', component: TopicComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- discuss/src/app/app.module.ts
+++ discuss/src/app/app.module.ts
@@ -4,17 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/partials/header/header.component';
+import { SidebarComponent } from './components/partials/sidebar/sidebar.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { PostComponent } from './components/pages/post/post.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { RegisterComponent } from './components/pages/register/register.component';
+import { CreatePostComponent } from './components/pages/create-post/create-post.component';
+import { TopicComponent } from './components/pages/topic/topic.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SidebarComponent,
+    HomeComponent,
+    PostComponent,
+    LoginComponent,
+    RegisterComponent,
+    CreatePostComponent,
+    TopicComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
